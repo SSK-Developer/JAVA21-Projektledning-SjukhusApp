68f8dbf3c29b8f7afb1b62fe275512a2d1627785
willzp <91528988+willzp@users.noreply.github.com>
2022-02-11 13:53:45 +0100

    Employees class

--- src/com/Sahlgrenska1/Employees.java
+++ src/com/Sahlgrenska1/Employees.java
@@ -1,4 +1,69 @@
 package com.Sahlgrenska1;
 
 public class Employees {
+
+    String name;
+    String password;
+    int salary;
+    int hours;
+    String speciality;
+    String patients;
+
+    public Employees(String name, String password, int salary, int hours, String speciality, String patients) {
+        this.name = name;
+        this.password = password;
+        this.salary = salary;
+        this.hours = hours;
+        this.speciality = speciality;
+        this.patients = patients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getPatients() {
+        return patients;
+    }
+
+    public void setPatients(String patients) {
+        this.patients = patients;
+    }
+
 }
