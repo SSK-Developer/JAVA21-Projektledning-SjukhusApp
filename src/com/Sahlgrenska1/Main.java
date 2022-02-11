42008ab3ca444f4bac44ab426e67d0760b9151bf
NoahNemhed <noahnemhed@hotmail.com>
2022-02-11 12:37:01 +0100

    a

--- src/com/Sahlgrenska1/Main.java
+++ src/com/Sahlgrenska1/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("wassup");
         System.out.println("wassup2");
         System.out.println("JagHeterNoah");
+        System.out.println("JagHeterNoah2");
     }
 }
