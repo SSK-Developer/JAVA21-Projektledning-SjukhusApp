fb089beb9316d9f48d789788e987d7413cf2b3e1
SSK-Developer <Saferkret@hotmail.com>
2022-02-11 13:04:43 +0100

    commit test1

--- src/com/Sahlgrenska1/Equipment.java
+++ src/com/Sahlgrenska1/Equipment.java
@@ -1,4 +1,33 @@
 package com.Sahlgrenska1;
 
 public class Equipment {
+
+    //Constructor
+    public Equipment(String name, String descriptions, int numOfAvailable) {
+        this.name = name;
+        this.descriptions = descriptions;
+        this.numOfAvailable = numOfAvailable;
+    }
+
+    //states
+    String name;
+    String descriptions;
+    int numOfAvailable;
+
+    //getters&setters
+    public String getName() {
+        return name;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public int getNumOfAvailable() {
+        return numOfAvailable;
+    }
+    public void setNumOfAvailable(int numOfAvailable) {
+        this.numOfAvailable = numOfAvailable;
+    }
+
 }
