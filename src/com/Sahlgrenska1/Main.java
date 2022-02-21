2f3d0d0692be1fa00d8d1e8bcc414894b798f37d
SSK-Developer <Saferkret@hotmail.com>
2022-02-21 13:10:53 +0100

    Skapat ny klass "Sahlgrenska"

--- src/com/Sahlgrenska1/Main.java
+++ src/com/Sahlgrenska1/Main.java
@@ -1,14 +1,23 @@
 package com.Sahlgrenska1;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("HEJ");
-        System.out.println("wassup");
-        System.out.println("wassup2");
-        System.out.println("JagHeterNoah");
-        System.out.println("JagHeterNoah2");
-        System.out.println("william");
+        Scanner sc = new Scanner(System.in);
+
+        Employees e1 = new Employees("safer","safer123",50000,50,"kirurg","null");
+        Employees e2 = new Employees("noah","noah123",50000,50,"kirurg","null");
+        Employees e3 = new Employees("hamood","hamood123",50000,50,"kirurg","null");
+        Employees e4 = new Employees("william","william123",50000,50,"kirurg","null");
+
+
+
+        System.out.println("Välkommen, vänligen fyll i inloggningsuppgifter nedan: ");
+        System.out.println("Namn: ");
+        String userLogInName = sc.nextLine();
+        System.out.println("Lösenord: ");
+        String userLogInPass = sc.nextLine();
+
     }
 }
